79b4083cdb3ffb6d65b11ebfec767dfcb76c7a37
Abhishek Potdar <abhirpotdar@gmail.com>
2022-12-29 03:06:44 +0530

    Get Contact Us API fix

--- application/models/ContactModel.php
+++ application/models/ContactModel.php
@@ -19,7 +19,9 @@ class ContactModel extends CI_Model
   	{
   		$this->db->select('iContactUsId  as contact_us_id,vName as name,vEmail as email,vPhone as phone,vSubject as subject,vMessage as message,dtAddedDate as added_date,eStatus as status');
 		$this->db->from('contact_us');
+		if(!empty($id)){
 		$this->db->where('iContactUsId ',$id);
+		}
 		$query_obj = $this->db->get();
 		$result = is_object($query_obj) ? $query_obj->result_array() : array();
 		return $result;
