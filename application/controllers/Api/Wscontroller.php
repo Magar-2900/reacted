88b419a912408e8eefa784b5c8d6939c1984c93c
Abhishek Potdar <abhirpotdar@gmail.com>
2023-02-16 11:52:00 +0530

    Stripe Webhook

--- application/controllers/Api/Wscontroller.php
+++ application/controllers/Api/Wscontroller.php
@@ -3382,14 +3382,14 @@ class Wscontroller extends REST_Controller
 		}
 	}
 
-	/*public function stripe_webhook_post(){
+	public function stripe_webhook_post(){
 		//echo 'hii';
 		// This is your Stripe CLI webhook secret for testing your endpoint locally.
 		\Stripe\Stripe::setApiKey('sk_test_51MMufsSBkuhk1xmVvwSrCUaHAgGUgUYH8ztbVGglv5wT4X60Gb927w7K8yP964Ilfk49yeWHXwDjZ6Vd8b83fXRB00ed7K8MeW');
 		$endpoint_secret = 'whsec_c0afa0a7f8319c5c31dd789424e3d9c255600697dedb50fad89df66db37ea63d';
 
 		$payload = @file_get_contents('php://input');
-		$sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+		$sig_header = print_r($_SERVER);
 		//echo $sig_header = print_r($this->input->request_headers());
 		$event = null;
 
@@ -3451,7 +3451,7 @@ class Wscontroller extends REST_Controller
 		default:
 			echo 'Received unknown event type ' . $event->type;
 		}
-	}*/
+	}
 
 	public function get_music_creator_individual_orders_get(){
 		try{
